357e39f24a736c63a523aafd1f282a729d0c666b
Mohimkhan <mohimkhan652@gmail.com>
2026-09-04 20:26:41 +0600

    style(Header): place the final logo and appName

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import { ThemeSwitcher } from "../ThemeSwitcher";
 import { Button } from "@/components/ui/button";
+import { appName } from "@/constants";
+
+/**
+ * TODO: Create Logo with the two existing persona, one in little tilt to left and another little tilt to right and both in circle, experiemnt with AI
+ * TODO: Tech Bro will explain with animation with canvas (first idea)
+ */
 
 const Header = () => {
   return (
@@ -11,17 +17,14 @@ const Header = () => {
       <Link href="/">
         <div className="relative h-10 w-48 sm:w-56 mt-2">
           <Image
-            src="/logo-text.png"
+            src="/final_personal_logo.png"
             alt="Persona AI"
             fill
-            className="hidden dark:block object-contain object-left"
+            className="block object-contain object-left"
           />
-          <Image
-            src="/logo-text-light.png"
-            alt="Persona AI"
-            fill
-            className="block dark:hidden object-contain object-left"
-          />
+          <div className="absolute font-semibold top-1/2 -translate-y-1/2 translate-x-12">
+            <h1>{appName}</h1>
+          </div>
         </div>
       </Link>
       <div className="flex items-center gap-4">
--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const LOGOUT_MODAL_ID: string = "my_logout_modal";
-export const appName: string = "QuickType";
+export const appName: string = "Persona AI";
 export const socialMediaHandle: string = "Mohim";
 export const socialProfileUrl: string = "https://github.com/Mohimkhan";
